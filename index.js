f282f6a3bdd29a5496ecbbde9cd0db3b0c80f5ae
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2021-12-15 13:52:36 +0100

    add mangoDB and add user

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const { title } = require('process')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const courseRoutes = require('./routes/courses')
 const cardRoutes = require('./routes/card')
 const app = express()
+const Handlebars = require('handlebars')
+const User = require('./modules/user')
 
 const hbs = exphbs.create({
-    defaultLayout:'main',
-    extname: 'hbs'
-})
+    defaultLayout: 'main', 
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+  });
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use( async (req,res,next) => {
+    try {
+    const user = await User.findById('61b9d0b6ab97d4fcdf12dcd4') 
+    req.user = user
+    next()
+    } catch (e) {
+     console.log(e)   
+    }
+
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:true}))
@@ -24,11 +40,31 @@ app.use('/add',addRoutes)
 app.use('/course',courseRoutes)
 app.use('/card',cardRoutes)
 
-
-
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`server is runinng on port ${PORT}`)
-})
+async function start(){
+    try {
+    const url = `mongodb+srv://akylbek:101191@cluster0.asi1a.mongodb.net/shop`
+    await mongoose.connect(url, {useNewUrlParser: true})
+    const candidate = await User.findOne()
+    if (!candidate) {
+        const user = new User({
+          email:'akylbekmuratbekuulu@gmail.com',
+          name:'Akylbek',
+          cart: {items: []}
+        })
+      await user.save()
+    }
+    app.listen(PORT, () => {
+        console.log(`server is runinng on port ${PORT}`)
+    })
+    }catch (e) {
+     console.log(e)
+    }
+}
+start()
+
+
+
+
 
--- routes/add.js
+++ routes/add.js
@@ -10,9 +10,21 @@ router.get('/', (req,res) => {
 })
 
 router.post('/', async (req,res) => {
-    const course = new Course(req.body.title, req.body.price, req.body.img)
-    await course.save()
-    res.redirect('/course')
+    // const course = new Course(req.body.title, req.body.price, req.body.img)
+    const course = new Course({
+        title: req.body.title,
+        price: req.body.price,
+        img: req.body.img,
+        userId: req.user
+    })
+
+    try {
+       await course.save()
+       res.redirect('/course')
+    } catch (e) {
+       console.log(e)
+    }
+ 
 })
 
 module.exports = router
