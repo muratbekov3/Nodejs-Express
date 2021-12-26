ec53df1cb28123d11ee7c2769ba6c044ac531cba
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2021-12-26 21:35:47 +0100

    adding CSRF-protection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const csrf = require('csurf')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
@@ -48,7 +49,7 @@ app.use(session({
   store
 
 }))
-
+app.use(csrf())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
