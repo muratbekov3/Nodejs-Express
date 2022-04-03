4befe27df6194e3f8160f2c948819800a4d6fb24
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2022-04-03 16:43:38 +0200

    reset email

--- emails/reset.js
+++ emails/reset.js
@@ -8,7 +8,7 @@ module.exports = function(email , token) {
     html: `
       <h1>Вы забыли пароль?</h1>
       <p>Ссылка для воотановления пароля</p>
-      <p><a href="${keys.BASE_URL}/auth/password/${token}"</p>
+      <p><a href="${keys.BASE_URL}/auth/password/${token}">Восстановить доступ</a></a></p>
       <hr />
       <a href="${keys.BASE_URL}">Akylbeks app</a>
     `
